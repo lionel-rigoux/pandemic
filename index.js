b2887737753185d8085d50c65d56253997feaf28
lionel.rigoux@sf.mpg.de <lionel.rigoux@gmail.com>
2018-02-02 16:13:46 +0100

    add some doc

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 #!/usr/bin/env node
 
-const prog = require('caporal');
-
+/* Load dependencies */
+/* ========================================================================== */
+// CLI framework
+const prog = require('caporal')
+// actions associated with respective commands
 const scaffold = require('./lib/scaffold');
 
+/* Program description */
+/* ========================================================================== */
+prog
+  .version('0.0.1')
+  .description('Academic writing without stress.')
+//  .logger(logger)
+
+/* Program actions */
+/* ========================================================================== */
 prog
   .command('scaffold', 'Create file tree according to the template')
-  .argument('<template>', 'Template to use')
+  .argument('<template>', 'Template to use (project | manuscript)')
   .option('--override', 'Write over existing files')
   .action(scaffold);
 
+prog
+  .command('publish', 'Compile a manuscript using pandoc')
+
+/* Put everithing to action */
+/* ========================================================================== */
 prog.parse(process.argv);
