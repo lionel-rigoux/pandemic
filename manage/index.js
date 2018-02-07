fc764c47c9508f1b7e08bf19611aa30ebef8d66f
lionel.rigoux@sf.mpg.de <lionel.rigoux@gmail.com>
2018-02-07 13:54:42 +0100

    add command to desinstall templates

--- manage/index.js
+++ manage/index.js
@@ -1,17 +1,24 @@
 const prog = require('caporal')
 const manageList = require('./manage-list.js')
 const manageInstall = require('./manage-install.js')
+const manageRemove = require('./manage-remove.js')
 
 module.exports = () => {
   prog
     .command('resource list', 'List installed templates')
-    .argument('<resource>', 'scaffolds or recipes', ['scaffolds','recipes'])
+    .argument('<resource>', 'scaffolds or recipes', ['scaffolds', 'recipes'])
     .action(manageList)
 
-prog
-  .command('resource install', 'Install new templates')
-  .argument('<resource>', 'scaffold or recipe', ['scaffold','recipe'])
-  .argument('<url>', 'Url of the template to install')
-  .option('--as <name>', 'Optional name for the template')
-  .action(manageInstall)
+  prog
+    .command('resource install', 'Install new template')
+    .argument('<resource>', 'scaffold or recipe', ['scaffold', 'recipe'])
+    .argument('<url>', 'Url of the template to install')
+    .option('--as <name>', 'Optional name for the template')
+    .action(manageInstall)
+
+  prog
+    .command('resource remove', 'Uninstall template')
+    .argument('<resource>', 'scaffold or recipe', ['scaffold', 'recipe'])
+    .argument('<name>', 'name of the template to uninstall')
+    .action(manageRemove)
 }
---
+++ manage/manage-remove.js
@@ -0,0 +1,24 @@
+const help = require('../lib/help.js')
+const resources = require('../lib/resources-tools.js')
+const fse = require('fs-extra')
+const path = require('path')
+
+module.exports = (args, options, logger) => {
+
+  let templatesDir = resources.getDir(args.resource)
+
+  // check if template exists
+  if (!resources.getTemplates(args.resource).includes(args.name)) {
+    logger.error(`Cannot find a ${args.resource} named "${args.name}".`)
+    help.dispResource(args.resource, logger)
+    process.exit(1)
+  }
+
+  // delete recipeFolder
+  let rFolder = path.join(templatesDir, args.name)
+  logger.info(`Uninstalling ${args.resource} "${args.name}"...`)
+  logger.debug(`Deleting ${rFolder}`)
+  fse.removeSync(rFolder);
+  logger.info('Sucess!')
+
+}
