4399e18cde283338658d70fa15bca6995fe1870e
Unknown <lionel.rigoux@sf.mpg.de>
2018-02-10 12:38:02 +0100

    us fs-extra for scaffolding

--- scaffold/index.js
+++ scaffold/index.js
@@ -2,15 +2,18 @@
 Copy a file tree from one of the package templates to current directory
  */
 
-const fs = require('fs')
-const ncp = require('ncp').ncp
+const fs = require('fs-extra')
 const config = require('../config.js')
 const path = require('path')
+const help = require('../lib/help.js')
+
+function _copyFilter(fileName) {
+  return path.basename(fileName) !== '.git'
+}
 
 module.exports = (args, options, logger) => {
   // get source and target folders
-  const templateRoot = config.SCAFFOLDS_PATH
-  const templatePath = path.join(templateRoot, args.template)
+  const templatePath = path.join(config.SCAFFOLDS_PATH, args.template)
   const localPath = process.cwd()
 
   // optional destructive copying
@@ -19,27 +22,28 @@ module.exports = (args, options, logger) => {
   // check if the template exists or give some help
   if (!fs.existsSync(templatePath)) {
     logger.error(`*** The requested template "${args.template}" wasn't found.`)
-    logger.info('\n Available templates: ')
-    fs.readdirSync(templateRoot)
-      .filter(file => !(/^\./g).test(file)) // ignore invisible
-      .forEach(file => {
-        logger.info(`  - ${file}`)
-      })
-    logger.info('\n')
+    help.dispScaffolds(logger)
     process.exit(1)
   }
 
   // start copy
   logger.info('Copying files...')
   var copyOptions = {
-    clobber: override
+    overwrite: override,
+    errorOnExist: true,
+    filter: _copyFilter
   }
-  ncp(templatePath, localPath, copyOptions, err => {
-    if (err) {
-      logger.error(`The files could not be copied (${err}).`)
-      process.exit(1)
-    }
-  })
+  try {
+    fs.copySync(
+      templatePath,
+      localPath,
+      copyOptions
+    )
+  } catch (err) {
+    logger.error('The files could not be copied:')
+    logger.error(` - ${err}`)
+    process.exit(1)
+  }
 
   // confirm success
   logger.info('The files have been copied!')
