a0eb84372ead49a05503959b0d8bfc004e43212b
lionel.rigoux@sf.mpg.de <lionel.rigoux@gmail.com>
2018-02-15 13:35:16 +0100

    standard linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,13 @@
-// http://eslint.org/docs/user-guide/configuring
-
 module.exports = {
-    root: true,
-    parserOptions: {
-        sourceType: "module",
-        parser: "babel-eslint"
-    },
-    env: {
-        browser: true
-    },
-    // https://github.com/standard/eslint-config-standard
-    // https://github.com/prettier/eslint-config-prettier
-    // https://github.com/vuejs/eslint-plugin-vue
-    extends: ["prettier", "prettier/standard", "plugin:vue/recommended"],
-    // vue required to lint *.vue files
-    // https://github.com/vuejs/eslint-plugin-vue
-    // https://github.com/prettier/eslint-plugin-prettier
-    plugins: ["vue", "prettier"],
-    rules: {
-        "prettier/prettier": "error"
-    }
-};
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  extends: [
+    'standard',
+  ]
+}
