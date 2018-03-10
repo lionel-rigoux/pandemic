867a378d65b3012b5a897e7cc3796abee86908b6
Unknown <lionel.rigoux@sf.mpg.de>
2018-03-10 12:50:48 +0100

    move pandoc to lib as publish

--- publish/index.js
+++ publish/index.js
@@ -1,35 +1,16 @@
-const fs = require('fs-extra');
-const pandoc = require('./pandoc.js');
+const publish = require('../lib/publish.js');
 const path = require('path');
-const loadInstructions = require('./load-instructions.js');
-const getYamlOptions = require('../lib/getYamlOptions.js');
 
 module.exports = (args, options, logger) => {
   // check that the source file exists
   const source = path.resolve(process.cwd(), args.source);
 
-  if (!fs.existsSync(source)) {
-    logger.error(`*** Could not find the source file ${args.source}.`);
-    process.exit(1);
-  }
-
-  // if no options provided, check yaml header
-  const yamlOptions = getYamlOptions(args.source);
-
-  // load recipe
-  const instructions = loadInstructions({
-    recipe: options.to || yamlOptions.recipe,
-    format: options.format || yamlOptions.format
-  });
-  logger.debug('Using recipe: ');
-  logger.debug(instructions);
-  logger.debug('');
-
   try {
     logger.info('Processing...');
-    pandoc({
+    publish({
       source,
-      instructions
+      recipe: options.to,
+      format: options.format
     });
     logger.info('Done!');
   } catch (err) {
