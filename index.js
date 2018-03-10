e61e12e5f80a2e4e9d310f90c8305b85c0f7a94d
lionel-rigoux <lionel.rigoux@sf.mpg.de>
2018-03-10 17:25:49 +0100

    export async publish

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const publish = require('./lib/publish.js');
 
 module.exports = {
-  publish
+  publish,
+  publishAsync: args => new Promise((resolve, reject) => {
+    try {
+      publish(args);
+      resolve(null);
+    } catch (err) {
+      reject(err);
+    }
+  })
 };
