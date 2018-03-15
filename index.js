3b4f31f611bab05c659c0b03528abadffd3e93a0
Unknown <lionel.rigoux@sf.mpg.de>
2018-03-15 10:11:47 +0100

    remove publishAsync

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 const publish = require('./lib/publish.js');
 
 module.exports = {
-  publish,
-  publishAsync: args => new Promise((resolve, reject) => {
-    try {
-      publish(args);
-      resolve(null);
-    } catch (err) {
-      reject(err);
-    }
-  })
+  publish
 };
